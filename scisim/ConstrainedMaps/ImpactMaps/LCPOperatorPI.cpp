5fc6ea422979096c010fe1712bc862971740cdb3
Kevin Wan <kevin.wan59049@gmail.com>
2022-02-05 22:08:39 -0500

    Add policy iteration class

--- scisim/ConstrainedMaps/ImpactMaps/LCPOperatorPI.cpp
+++ scisim/ConstrainedMaps/ImpactMaps/LCPOperatorPI.cpp
@@ -1,5 +1,38 @@
 //
-// Created by kevin on 2022-02-01.
+// Created by Kevin Wan on 2022-02-01.
 //
 
 #include "LCPOperatorPI.h"
+#include "scisim/Utilities.h"
+
+LCPOperatorPI::LCPOperatorPI(const scalar &tol)
+: m_tol (tol)
+{
+    assert(m_tol >= 0);
+}
+
+LCPOperatorPI::LCPOperatorPI(std::istream &input_stream)
+: m_tol (Utilities::deserialize<scalar>( input_stream ))
+{
+    assert(m_tol >= 0);
+}
+
+void LCPOperatorPI::flow(const std::vector<std::unique_ptr<Constraint>> &cons, const SparseMatrixsc &M,
+                         const SparseMatrixsc &Minv, const VectorXs &q0, const VectorXs &v0, const VectorXs &v0F,
+                         const SparseMatrixsc &N, const SparseMatrixsc &Q, const VectorXs &nrel, const VectorXs &CoR,
+                         VectorXs &alpha) {
+
+}
+
+std::string LCPOperatorPI::name() const {
+    return "lcp_policy_iteration";
+}
+
+std::unique_ptr<ImpactOperator> LCPOperatorPI::clone() const {
+    return std::unique_ptr<ImpactOperator>(new LCPOperatorPI(m_tol));
+}
+
+void LCPOperatorPI::serialize(std::ostream &output_stream) const {
+    Utilities::serialize(m_tol, output_stream);
+}
+
--- scisim/ConstrainedMaps/ImpactMaps/LCPOperatorPI.h
+++ scisim/ConstrainedMaps/ImpactMaps/LCPOperatorPI.h
@@ -1,14 +1,32 @@
 //
-// Created by kevin on 2022-02-01.
+// Created by Kevin Wan on 2022-02-01.
 //
 
-#ifndef SCISIM_LCPOPERATORPI_H
-#define SCISIM_LCPOPERATORPI_H
+#ifndef SCISIM_LCP_OPERATOR_PI_H
+#define SCISIM_LCP_OPERATOR_PI_H
 
+#include "ImpactOperator.h"
 
-class LCPOperatorPI {
+class LCPOperatorPI final : public ImpactOperator {
 
+public:
+
+    explicit LCPOperatorPI(const scalar& tol);
+    explicit LCPOperatorPI( std::istream& input_stream );
+
+    virtual ~LCPOperatorPI() override = default;
+
+    virtual void flow( const std::vector<std::unique_ptr<Constraint>>& cons, const SparseMatrixsc& M, const SparseMatrixsc& Minv, const VectorXs& q0, const VectorXs& v0, const VectorXs& v0F, const SparseMatrixsc& N, const SparseMatrixsc& Q, const VectorXs& nrel, const VectorXs& CoR, VectorXs& alpha ) override;
+
+    virtual std::string name() const override;
+
+    virtual std::unique_ptr<ImpactOperator> clone() const override;
+
+    virtual void serialize( std::ostream& output_stream ) const override;
+
+private:
+    const scalar m_tol;
 };
 
 
-#endif //SCISIM_LCPOPERATORPI_H
+#endif //SCISIM_LCP_OPERATOR_PI_H
